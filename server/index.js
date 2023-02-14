73b0189d24c555c58bef69ead66860a028d827e3
Jpduker <jayaprakashnis619@gmail.com>
2023-02-14 21:50:51 +0530

    added index.js in server folder

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-import express form "express";
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/home", async (req, res) => {
+  res.send("hello from DALL-E");
+});
+
+const startServer = async () => {
+  app.listen(8080, () =>
+    console.log("server has started on port http://localhost:8080 ")
+  );
+};
+
+startServer();
