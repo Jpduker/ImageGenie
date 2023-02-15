0c20b2821bd59f6f942d3f74e59b1a0da514724c
Jpduker <jayaprakashnis619@gmail.com>
2023-02-15 17:20:53 +0530

    commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import * dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import {Configuration , OpenAIApi} from 'openai';
 
 
 dotenv.config();
 
 const router = express.Router();
+
+ 
